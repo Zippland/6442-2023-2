bb9350e2dc9799bb443c28383ae31a329d574b26
Zylan <126135306+Zippland@users.noreply.github.com>
2024-06-10 04:46:23 +1000

    Update TerminalNode.java

--- src/Q2/TerminalNode.java
+++ src/Q2/TerminalNode.java
@@ -13,15 +13,22 @@ public class TerminalNode extends Node {
     public String classify(Spider spider) {
         // TODO - Your Code Starts Here (Q2a)
         // TODO - Note: you may implement this function and/or classify in the subclasses
-
-        return null; // TODO replace this line
+        return outcome;
         // TODO - Your Code END Here
     }
 
     public String getOutcome(){
+        System.out.println("Terminal");
         return outcome;
     }
 
-
+    @Override
+    public String toStringHelp(int level) {
+        // TODO - Your Code Starts Here (Q2b)
+        // You may create helper functions in any of the Node classes
+        // You may wish to use the method Spider.propertyToString
+        return outcome+"\n"; // TODO replace this line
+        // TODO - Your Code END Here
+    }
 
 }
